b19fdd68878d60a89da4ddf776b653661e67a743
genichiro22 <scepterjw@yahoo.co.jp>
2023-05-29 13:00:45 +0900

    2 dimension

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ class Agent(nn.Module):
     def __init__(self):
         super(Agent, self).__init__()
         self.fc = nn.Sequential(
-            nn.Linear(2, 128),
+            nn.Linear(4, 128),
             nn.ReLU(),
-            nn.Linear(128, 2)
+            nn.Linear(128, 4)
         )
 
     def forward(self, state):
@@ -16,25 +16,29 @@ class Agent(nn.Module):
 import numpy as np
 
 class Environment:
-    def __init__(self, size=10):
+    def __init__(self, size=5):
         self.size = size
         self.reset()
 
     def reset(self):
-        self.agent_pos = np.random.randint(self.size)
-        self.destination_pos = np.random.randint(self.size)
+        self.agent_pos = np.random.randint(self.size, size=2)  # 2D position
+        self.destination_pos = np.random.randint(self.size, size=2)  # 2D position
         return self.get_state()
 
     def get_state(self):
-        return np.array([self.agent_pos, self.destination_pos])
+        return np.concatenate([self.agent_pos, self.destination_pos])
 
     def step(self, action):
         if action == 0:  # Move left
-            self.agent_pos = max(0, self.agent_pos - 1)
+            self.agent_pos[1] = max(0, self.agent_pos[1] - 1)
         elif action == 1:  # Move right
-            self.agent_pos = min(self.size - 1, self.agent_pos + 1)
-        done = self.agent_pos == self.destination_pos
-        reward = 1 if done else -1
+            self.agent_pos[1] = min(self.size - 1, self.agent_pos[1] + 1)
+        elif action == 2:  # Move up
+            self.agent_pos[0] = max(0, self.agent_pos[0] - 1)
+        elif action == 3:  # Move down
+            self.agent_pos[0] = min(self.size - 1, self.agent_pos[0] + 1)
+        done = np.array_equal(self.agent_pos, self.destination_pos)
+        reward = 1 if done else -0.1
         return self.get_state(), reward, done
 
 import torch.optim as optim
@@ -47,7 +51,7 @@ optimizer = optim.Adam(agent.parameters())
 env = Environment()
 
 # Training loop
-for episode in range(1000):
+for episode in range(10000):
     state = env.reset()
     log_probs = []
     rewards = []
@@ -76,23 +80,28 @@ for episode in range(1000):
     optimizer.step()
 
     # Print progress
-    if episode % 10 == 0:
+    if episode % 100 == 0:
         print(f"Episode {episode}: loss = {loss.item()}")
 
-def print_env(env, agent_pos, destination_pos):
+torch.save(agent.state_dict(), 'agent_model.pth')
+
+def print_env(env):
     for i in range(env.size):
-        if i == agent_pos:
-            print('A', end='')
-        elif i == destination_pos:
-            print('D', end='')
-        else:
-            print('-', end='')
-    print()
+        for j in range(env.size):
+            if np.array_equal([i, j], env.agent_pos):
+                print('A', end=' ')
+            elif np.array_equal([i, j], env.destination_pos):
+                print('D', end=' ')
+            else:
+                print('-', end=' ')
+        print()  # newline at the end of each row
+    print()  # newline to separate each step
+
 
 # Testing loop
 with torch.no_grad():
     state = env.reset()
-    print_env(env, env.agent_pos, env.destination_pos)
+    print_env(env)
     done = False
     while not done:
         # Select action
@@ -104,4 +113,4 @@ with torch.no_grad():
         state, reward, done = env.step(action.item())
 
         # Print current state
-        print_env(env, env.agent_pos, env.destination_pos)
+        print_env(env)
